325fc4f01a79d4a3e70e450501d207369f399804
Dastin Sandura <d.kuwalek@gmail.com>
2024-05-26 19:17:30 +0200

    Add possibility of passing parameters - two ingredients to generate a recipe

--- src/main/java/sandura/mhdatabase/servlet/DatabaseServlet.java
+++ src/main/java/sandura/mhdatabase/servlet/DatabaseServlet.java
@@ -11,25 +11,53 @@ import sandura.mhdatabase.kitchen.RecipeRepository;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
-        PrintWriter writer = resp.getWriter();
+        req.getQueryString();
+        String first = req.getParameter("first");
+        String second = req.getParameter("second");
 
+        //        super.doGet(req, resp);
+        PrintWriter writer = resp.getWriter();
+        writer.println("First parameter %s".formatted(first));
+        writer.println("Second parameter %s".formatted(second));
+
+        FelyneRecipesService var3 = new FelyneRecipesService();
+
+        if (first != null && second != null) {
+            List<String> requestParameters = new ArrayList<>();
+            requestParameters.add(first);
+            requestParameters.add(second);
+            writer.println("Possible ingredient from " + requestParameters);
+            writer.println(var3.getPossibleIngredientPairs(requestParameters).toString());
+
+            writer.println();
+
+        }
         ItemRepository var1 = new ItemRepository();
         RecipeRepository var2 = new RecipeRepository();
         writer.println(var2.generatePossibleRecipes("Cubesteak", "Hardtack", 2));
         writer.println("Contents of Items Repository");
         writer.println(var1.getItems());
-        FelyneRecipesService var3 = new FelyneRecipesService();
         ArrayList var4 = new ArrayList();
         var4.add("Chili Cheese");
         var4.add("Wild Wonton");
         writer.println("Possible ingredient from " + var4);
         writer.println(var3.getPossibleIngredientPairs(var4).toString());
         var3.getNumberOfCooksFromIngredient("Chili Cheese");
+
+        writer.println("""
+        <form action="" method"get">
+            <div>
+                <label for="first">First ingredient</label>
+                <input type="text" name="first" id="first"/>
+            </div>
+            <div>
+            </div>
+        </form>""");
 
     }
 
